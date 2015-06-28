41e101d2b0be478c15e37a04e6bd4465c490a237
Michael Fix <mfix@wisc.edu>
2015-06-28 11:44:29 -0700

    Add form, fix close button

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,20 @@
 // $('[data-toggle="tooltip"]').tooltip();
+// var open = function(elem){
+//     $(elem).unbind("click");
+//     $('#shadow').show();
+//     $('.help-text').hide();
+//     $(elem).addClass('active');
+//     $(elem).removeClass('inactive');
+//     $(elem).animate({
+//         top: '50%',
+//         left: '50%',
+//         width: '500px',
+//         height: '300px'
+//     }, function(){
+//         //Maybe don't make this a callback
+//         $('#helpbox').show();
+//     });
+// };
 var close = function(){
     $('#helpbox').hide();
     $('#shadow').hide();
@@ -11,14 +27,16 @@ var close = function(){
         $('.help').removeClass('active');
         $('.help').addClass('inactive');
         $('.help-text').show();
+        $('.help').bind("click", open);
     });
 };
-var open = function(elem){
+var open = function(){
+    $('.help').unbind("click");
     $('#shadow').show();
     $('.help-text').hide();
-    $(elem).addClass('active');
-    $(elem).removeClass('inactive');
-    $(elem).animate({
+    $('.help').addClass('active');
+    $('.help').removeClass('inactive');
+    $('.help').animate({
         top: '50%',
         left: '50%',
         width: '500px',
@@ -28,13 +46,12 @@ var open = function(elem){
         $('#helpbox').show();
     });
 };
-$('.help.inactive').click(function(){
-    open(this);
-});
-$('#shadow').click(function(){
-    close();
-});
-// $('#x').click(function(){
-//     close();
-// })
+$('.help.inactive').click(open);
+$('#shadow').click(close);
+$('#x').click(close);
+// $('#messenger-submit').click(function(){
+//     var message = "mailto:mrfix84@gmail.com?subject=hello&body=";
+//     message += document.getElementById("message").value;
+//     window.open(message, '_blank');
+// });
 
